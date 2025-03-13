e88117f6530e1b7749a715d426e33904c974ed5e
SARAN <92285228+Saran9965@users.noreply.github.com>
2025-03-13 11:07:08 +0530

    Add files via upload

---
+++ project/project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.signup,name='signup'),
+    path('login/', views.loginpage,name='login'),
+    path('home/', views.HomePage,name='home'),
+    path('logout/', views.logoutpage,name='logout'),
+]
+
